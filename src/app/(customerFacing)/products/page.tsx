3cdc8a0bf816d51061e408b3d06b8ccccf91776e
Paulina <paulina.kolasa@espeo.eu>
2024-07-15 15:37:20 +0200

    add products page

---
+++ src/app/(customerFacing)/products/page.tsx
@@ -0,0 +1,39 @@
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import db from "@/db/db";
+import { Suspense } from "react";
+
+export default function ProductsPage() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductsSuspense />
+      </Suspense>
+    </div>
+  );
+}
+
+const getProducts = () => {
+  return db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { name: "asc" },
+  });
+};
+
+const ProductsSuspense = async () => {
+  const products = await getProducts();
+
+  return products.map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
+};
