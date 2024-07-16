9dc0ecb5641c2aa844ae59d4acf24679387820ec
Paulina <paulina.kolasa@espeo.eu>
2024-07-16 15:13:40 +0200

    add checkout form

---
+++ src/app/(customerFacing)/products/[id]/purchase/components/CheckoutForm.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import { useOrderExists } from "@/app/actions/orders";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { formatCurrency } from "@/lib/formatter";
+import {
+  Elements,
+  useStripe,
+  useElements,
+  PaymentElement,
+  LinkAuthenticationElement,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
+import { FormEvent, useState } from "react";
+
+type CheckoutFormProps = {
+  product: {
+    imagePath: string;
+    price: number;
+    name: string;
+    description: string;
+    id: string;
+  };
+  clientSecret: string;
+};
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
+);
+
+export const CheckoutForm = ({ product, clientSecret }: CheckoutFormProps) => {
+  return (
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image
+            src={product.imagePath}
+            fill
+            alt={product.name}
+            className="object-cover"
+          />
+        </div>
+        <div>
+          <div>
+            <div className="text-lg">{formatCurrency(product.price / 100)}</div>
+          </div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">
+            {product.description}
+          </div>
+        </div>
+      </div>
+
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form price={product.price} productId={product.id} />
+      </Elements>
+    </div>
+  );
+};
+
+const Form = ({ price, productId }: { price: number; productId: string }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [email, setEmail] = useState<string>("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (stripe == null || elements == null || email == null) return;
+
+    setIsLoading(true);
+
+    const orderExist = await useOrderExists(email, productId);
+
+    if (orderExist) {
+      setErrorMessage("You have already purchased this product.");
+      setIsLoading(false);
+      return;
+    }
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === "card_error" || error.type === "validation_error") {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage("An unknown error occurred. Please try again later.");
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          {errorMessage && (
+            <CardDescription className="text-destructive">
+              {errorMessage}
+            </CardDescription>
+          )}
+        </CardHeader>
+        <CardContent>
+          <PaymentElement />
+          <div className="mt-4">
+            <LinkAuthenticationElement
+              onChange={(e) => setEmail(e.value.email)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={stripe == null || elements == null || isLoading}
+          >
+            {isLoading
+              ? "Purchasing..."
+              : `Purchase - ${formatCurrency(price / 100)}`}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+};
