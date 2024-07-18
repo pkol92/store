9ebbe3395973651822c9e2785a54e90680129366
Paulina <paulina.kolasa@espeo.eu>
2024-07-18 12:12:37 +0200

    add success page

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,3 +1,79 @@
-export default function SuccessPage() {
-  return <h1>success</h1>;
+import { Button } from "@/components/ui/button";
+import db from "@/db/db";
+import { formatCurrency } from "@/lib/formatter";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export default async function SuccessPage({
+  searchParams,
+}: {
+  searchParams: { payment_intent: string };
+}) {
+  const paymentIntent = await stripe.paymentIntents.retrieve(
+    searchParams.payment_intent
+  );
+
+  if (paymentIntent.metadata.productId == null) return notFound();
+
+  const product = await db.product.findUnique({
+    where: { id: paymentIntent.metadata.productId },
+  });
+
+  if (product == null) return notFound();
+
+  const isSuccess = paymentIntent.status === "succeeded";
+  return (
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <h1 className="text-4xl font-bold">
+        {isSuccess ? "Success!" : "Error!"}
+      </h1>
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image
+            src={product.imagePath}
+            fill
+            alt={product.name}
+            className="object-cover"
+          />
+        </div>
+        <div>
+          <div>
+            <div className="text-lg">{formatCurrency(product.price / 100)}</div>
+          </div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">
+            {product.description}
+          </div>
+          <Button className="mt-4" size="lg" asChild>
+            {isSuccess ? (
+              <a
+                href={`/products/download/${await createDownloadVerification(
+                  product.id
+                )}`}
+              >
+                Download
+              </a>
+            ) : (
+              <Link href={`/products/${product.id}/purchase`}>Try again</Link>
+            )}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const createDownloadVerification = async (productId: string) => {
+  const next24Hours = new Date(Date.now() + 1000 * 60 * 60 * 24);
+  return (
+    await db.downloadVerification.create({
+      data: {
+        productId,
+        expiresAt: next24Hours,
+      },
+    })
+  ).id;
+};
